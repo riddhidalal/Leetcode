71b2a512d7d2c38e1fea5239ef133a82129fa66c
Riddhi Dalal <124533399+riddle-me@users.noreply.github.com>
2023-09-09 21:45:28 +0530

    Time: 35 ms (90.70%) | Memory: 14.2 MB (32.43%) - LeetSync

--- easy/1-two-sum/two-sum.py
+++ easy/1-two-sum/two-sum.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        result = []
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j :
-                    if nums[i] + nums[j] == target:
-                        if i and j not in result:
-                            result.append(i)
-                            result.append(j)
-        return result
-                    
+        hashmap = {} #v:n
+        
+        for val,num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], val]
+            hashmap[num]  = val
+        return
+                
         
